16e7394114e54aa0ed3cd4f96edb9e19a034c356
unknown <luka.todorovic2001@gmail.com>
2023-05-05 14:16:12 +0200

    CreateTour validation button and lists

--- SIMS-Tourist-Project-main/Tourist-Project/Tourist-Project/App.xaml.cs
+++ SIMS-Tourist-Project-main/Tourist-Project/Tourist-Project/App.xaml.cs
@@ -5,7 +5,18 @@ namespace Tourist_Project
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App
+    public partial class App : Application
     {
+        public void ChangeLanguage(string currLang)
+        {
+            if (currLang.Equals("en-US"))
+            {
+                TranslationSource.Instance.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            }
+            else
+            {
+                TranslationSource.Instance.CurrentCulture = new System.Globalization.CultureInfo("sr-LATN");
+            }
+        }
     }
 }

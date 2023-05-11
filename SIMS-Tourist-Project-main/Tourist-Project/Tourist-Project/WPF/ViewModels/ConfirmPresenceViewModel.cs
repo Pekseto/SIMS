ba86506f13ade9ca1e219ad27039ba2017d469af
Pekseto <lukazecevic5@gmail.com>
2023-05-11 16:45:50 +0200

    fixed minor details for notifications

--- SIMS-Tourist-Project-main/Tourist-Project/Tourist-Project/WPF/ViewModels/ConfirmPresenceViewModel.cs
+++ SIMS-Tourist-Project-main/Tourist-Project/Tourist-Project/WPF/ViewModels/ConfirmPresenceViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
 using Tourist_Project.Applications.UseCases;
 using Tourist_Project.Domain.Models;
 
@@ -9,6 +10,17 @@ namespace Tourist_Project.WPF.ViewModels
         private readonly NotificationGuestTwoService notificationService = new();
         private readonly TourAttendanceService attendanceService = new();
         private readonly TourService tourService = new();
+        private Message message;
+        public Message Message
+        {
+            get => message;
+            set
+            {
+                if (value == message) return;
+                message = value;
+                OnPropertyChanged(nameof(Message));
+            }
+        }
         public string Text { get; set; }
         public Tour Tour { get; set; }
         public NotificationGuestTwo Notification { get; set; }
@@ -19,11 +31,12 @@ namespace Tourist_Project.WPF.ViewModels
         public ConfirmPresenceViewModel(NotificationGuestTwo notification)
         {
             Notification = notification;
+            Message = notification.Responded ? new Message(true, "Thank you for your response") : new Message();
 
             TourAttendance = attendanceService.GetByTourIdAndUserId(notification.TourId, notification.UserId);
             Tour = tourService.GetOne(notification.TourId);
 
-            Text = "The tour guide has called you out for the tour " + Tour.Name + ". Please confirm if you are present by clicking on the Yes button, or No otherwise!";
+            Text = "The tour guide has called you out for the tour " + Tour.Name + ".\nPlease confirm if you are present by clicking on the Yes button, or No otherwise!";
 
 
             YesCommand = new RelayCommand(OnYesClick, () => !Notification.Responded);
@@ -34,6 +47,7 @@ namespace Tourist_Project.WPF.ViewModels
         {
             TourAttendance.Presence = Presence.No;
             Notification.Responded = true;
+            Message = new Message(true, "Thank you for your response");
 
             attendanceService.Update(TourAttendance);
             notificationService.Update(Notification);
@@ -43,6 +57,7 @@ namespace Tourist_Project.WPF.ViewModels
         {
             TourAttendance.Presence = Presence.Yes;
             Notification.Responded = true;
+            Message = new Message(true, "Thank you for your response");
 
             attendanceService.Update(TourAttendance);
             notificationService.Update(Notification);

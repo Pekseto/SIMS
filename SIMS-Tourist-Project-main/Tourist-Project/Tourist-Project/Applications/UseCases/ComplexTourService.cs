d32025c3ddd47a37eb41715f9ff3a32fabc6a884
Pekseto <lukazecevic5@gmail.com>
2023-05-28 17:45:40 +0200

    added all back stuff related to complex tours

---
+++ SIMS-Tourist-Project-main/Tourist-Project/Tourist-Project/Applications/UseCases/ComplexTourService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tourist_Project.Domain.Models;
+using Tourist_Project.Domain.RepositoryInterfaces;
+using Tourist_Project.Serializer;
+
+namespace Tourist_Project.Applications.UseCases
+{
+    public class ComplexTourService
+    {
+        private static readonly Injector injector = new();
+        private readonly IComplexTourRepository complexTourRepository = injector.CreateInstance<IComplexTourRepository>();
+        public ComplexTourService()
+        {
+        }
+
+        public List<ComplexTour> GetAll()
+        {
+            return complexTourRepository.GetAll();
+        }
+
+        public List<ComplexTour> GetAllForUser(int userId)
+        {
+            return complexTourRepository.GetAllForUser(userId);
+        }
+
+        public ComplexTour Save(ComplexTour complexTour)
+        {
+            return complexTourRepository.Save(complexTour);
+        }
+    }
+}
---
+++ SIMS-Tourist-Project-main/Tourist-Project/Tourist-Project/Repositories/ComplexTourRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tourist_Project.Domain.Models;
+using Tourist_Project.Domain.RepositoryInterfaces;
+using Tourist_Project.Serializer;
+
+namespace Tourist_Project.Repositories
+{
+    public class ComplexTourRepository : IComplexTourRepository
+    {
+        private const string filePath = "../../../Data/complexTours.csv";
+        private readonly Serializer<ComplexTour> serializer;
+        public List<ComplexTour> Tours;
+
+        public ComplexTourRepository()
+        {
+            serializer = new Serializer<ComplexTour>();
+            Tours = serializer.FromCSV(filePath);
+        }
+        public List<ComplexTour> GetAll()
+        {
+            return serializer.FromCSV(filePath);
+        }
+
+        public List<ComplexTour> GetAllForUser(int userId)
+        {
+            Tours = GetAll();
+            return Tours.Where(tour => tour.UserId == userId).ToList();
+        }
+
+        public ComplexTour Save(ComplexTour complexTour)
+        {
+            Tours = GetAll();
+            complexTour.Id = NextId();
+            Tours.Add(complexTour);
+            serializer.ToCSV(filePath, Tours);
+            return complexTour;
+        }
+
+        public int NextId()
+        {
+            if (Tours.Count < 1)
+            {
+                return 1;
+            }
+            return Tours.Max(t => t.Id) + 1;
+        }
+    }
+}

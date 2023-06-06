e7e688835274629b7caaad79517e3f898d952654
Pekseto <lukazecevic5@gmail.com>
2023-06-06 20:01:35 +0200

    finished vouchers report

--- SIMS-Tourist-Project-main/Tourist-Project/Tourist-Project/WPF/ViewModels/VouchersViewModel.cs
+++ SIMS-Tourist-Project-main/Tourist-Project/Tourist-Project/WPF/ViewModels/VouchersViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Tourist_Project.Applications.UseCases;
 using Tourist_Project.Domain.Models;
 
@@ -12,11 +13,46 @@ namespace Tourist_Project.WPF.ViewModels
     public class VouchersViewModel : ViewModelBase
     {
         private readonly TourVoucherService voucherService = new();
+        private int userId;
+        private Message message;
+
         public ObservableCollection<TourVoucher> Vouchers { get; set; }
+        public ICommand DownloadPDFCommand { get; set; }
+
+        public Message Message
+        {
+            get => message;
+            set
+            {
+                message = value;
+                OnPropertyChanged();
+            }
+        }
 
         public VouchersViewModel(User user)
         {
+            userId = user.Id;
             Vouchers = new ObservableCollection<TourVoucher>(voucherService.GetAllForUser(user.Id));
+            DownloadPDFCommand = new RelayCommand(DownloadPDFClick, () => true);
+        }
+
+        private void DownloadPDFClick()
+        {
+            if (voucherService.GeneratePDFReport(userId))
+            {
+                _ = ShowMessageAndHide(new Message(true, "PDF report created successfully"));
+            }
+            else
+            {
+                _ = ShowMessageAndHide(new Message(false, "Error while generating PDF report"));
+            }
+        }
+
+        private async Task ShowMessageAndHide(Message message)
+        {
+            Message = message;
+            await Task.Delay(5000);
+            Message = new Message();
         }
     }
 }
